0524e4994ffe3240d9b2488380bae9e8f50d0b82
59013-81-12 <59013-81-12@exchange.ad.afpanet>
2022-04-26 16:30:43 +0200

    Skeleton

---
+++ src/test/java/fr/vincent/testRest/TestRestApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vincent.testRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
